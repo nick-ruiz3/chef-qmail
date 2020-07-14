218a2cee5923685a7c60aabb8d02e323092ef898
nick-ruiz3 <nruiz@ezlinks.com>
2020-07-14 12:56:55 -0500

    update qmail dir

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['qmail']['src_packager'] = '/usr/local/src'
-default['qmail']['qmail_home'] = '/var/qmail'
+default['qmail']['qmail_home'] = '/var/lib/qmail'
 default['qmail']['qmail_log'] = '/var/log/qmail'
 default['qmail']['qmail_service'] = '/service'
 default['qmail']['qmail_bals'] = '/data/mail'
