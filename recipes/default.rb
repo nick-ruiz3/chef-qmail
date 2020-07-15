fcda8183525ea967a3eb45adffb3bbf27336b84a
nick-ruiz3 <nruiz@ezlinks.com>
2020-07-15 09:39:47 -0500

    Try different source

--- recipes/default.rb
+++ recipes/default.rb
@@ -176,7 +176,7 @@ bash 'download-compilation-qmail-src-ldap' do
   user 'root'
   cwd node['qmail']['src_packager']
   code <<-EOH
-  git clone  https://github.com/stephaneLII/qmail-src-ldap.git
+  git clone  https://github.com/sitle/qmail-src-ldap.git
   cd qmail-src-ldap
   make setup check
   chmod a+x #{config_fast_command}
@@ -187,7 +187,7 @@ end
 ##################################
 # Creation du script de controle qmailctl
 ##################################
- 
+
  directory "#{qmail_home}/bin" do
    owner 'root'
    group 'qmail'
